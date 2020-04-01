6b1682c9a787cb3de5b85d6728537a92334f8c25
Yossi <yygold@gmail.com>
2020-03-31 21:04:55 -0400

    Refactoring routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
-Rails.application.routes.draw do
+# Rails.application.routes.draw do
+#   root 'pictures#index'
 
-  root 'pictures#index'
-    get 'pictures' => 'pictures#index'
-    
-    post 'pictures' => 'pictures#create' 
-    get 'pictures/new' => 'pictures#new' 
-    get 'pictures/:id' => 'pictures#show', as: 'picture'
-    
-    get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-    patch 'pictures/:id' => "pictures#update"
-
-    delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
-
+#   get 'pictures' => 'pictures#index'
+#   get 'pictures/new' => 'pictures#new'
+#   get 'pictures/:id/edit' => 'pictures#edit', as: 'edit_picture'
+#   get 'pictures/:id' => 'pictures#show', as: 'picture'
   
+#   post 'pictures' => "pictures#create"
+#   patch 'pictures/:id' => "pictures#update"
+#   delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
 
+# end
 
+Rails.application.routes.draw do
+  root 'pictures#index'
+  
+  resources :pictures
 end
